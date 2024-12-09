2561eb4cde05430f236e1aa7c08d23466bd64f8f
kkkkimtaehyeon <kth1927@naver.com>
2024-12-09 10:28:46 +0900

    init

---
+++ src/test/java/com/nhnacademy/shoppingmallservice/ShoppingmallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.shoppingmallservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
