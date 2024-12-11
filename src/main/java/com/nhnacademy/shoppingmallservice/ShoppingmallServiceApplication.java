20b34db05dc0a0681c61d4bc2cfa90599e09984f
choegwanghyeog <fkqlaus@naver.com>
2024-12-11 11:19:31 +0900

    인증서버 게이트웨이 통신

--- src/main/java/com/nhnacademy/shoppingmallservice/ShoppingmallServiceApplication.java
+++ src/main/java/com/nhnacademy/shoppingmallservice/ShoppingmallServiceApplication.java
@@ -3,9 +3,11 @@ package com.nhnacademy.shoppingmallservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient // 유레카 클라이언트 활성화
+@EnableDiscoveryClient
+@EnableFeignClients  // 유레카 클라이언트 활성화
 public class ShoppingmallServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/nhnacademy/shoppingmallservice/properties/PropertiesBase.java
+++
@@ -1,4 +0,0 @@
-package com.nhnacademy.shoppingmallservice.properties;
-
-public interface PropertiesBase {
-}
