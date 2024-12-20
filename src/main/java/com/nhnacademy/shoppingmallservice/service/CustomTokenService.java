cc2d4b222baf6eb9dfb48c6a7b30c7ce89105b1e
kkkkimtaehyeon <kth1927@naver.com>
2024-12-20 18:05:10 +0900

    액세스토큰 프론트에서 저장하도록 전달만 함

--- src/main/java/com/nhnacademy/shoppingmallservice/service/CustomTokenService.java
+++ src/main/java/com/nhnacademy/shoppingmallservice/service/CustomTokenService.java
@@ -5,7 +5,7 @@ import com.nhnacademy.shoppingmallservice.enums.TokenType;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface CustomTokenService {
-    void issueJwt(HttpServletResponse res, MemberDto memberDto);
+    String issueJwt(HttpServletResponse res, MemberDto memberDto);
     String reissueAccessToken(String email);
     void saveTokenOnCookie(HttpServletResponse response, TokenType type, String value);
 }
