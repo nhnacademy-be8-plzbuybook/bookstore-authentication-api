2c844d11fcd4230eb03c4831af7b6130ba86e575
yunjiho <yoonwlgh12@gmail.com>
2024-12-28 17:49:32 +0900

    yml에서 암호화 된 정보를 불러오는 곳 입니다.

---
+++ src/main/java/com/nhnacademy/shoppingmallservice/skm/properties/SKMProperties.java
@@ -0,0 +1,29 @@
+package com.nhnacademy.shoppingmallservice.skm.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app.skm")
+public class SKMProperties {
+    private String url;
+    private String appKey;
+    private String keystoreFile;
+    private String password;
+    private Redis redis;
+
+
+    @Getter
+    @Setter
+    public static class Redis{
+       private String host;
+       private String port;
+       private String password;
+       private String range;
+   }
+}
+
