8ef488aa5a45ae8655dacd8caf612f34ca79adc3
yunjiho <yoonwlgh12@gmail.com>
2024-12-28 17:46:51 +0900

    skm을 해독할 때 예외 처리를 해줬습니다.

---
+++ src/main/java/com/nhnacademy/shoppingmallservice/skm/exception/KeyMangerException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.shoppingmallservice.skm.exception;
+
+public class KeyMangerException extends RuntimeException {
+    public KeyMangerException(String message) {
+        super(message);
+    }
+}
