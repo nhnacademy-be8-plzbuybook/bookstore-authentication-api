2fb2a122002de85aa11388689c72283b89d93fd0
guksy <todus723@gmail.com>
2025-01-07 08:58:33 +0900

    마지막 로그인 일시 업데이트

--- src/main/java/com/nhnacademy/shoppingmallservice/controller/AuthController.java
+++ src/main/java/com/nhnacademy/shoppingmallservice/controller/AuthController.java
@@ -3,16 +3,22 @@ package com.nhnacademy.shoppingmallservice.controller;
 import com.nhnacademy.shoppingmallservice.dto.LoginRequestDto;
 import com.nhnacademy.shoppingmallservice.dto.LoginResponseDto;
 import com.nhnacademy.shoppingmallservice.dto.MemberDto;
+import com.nhnacademy.shoppingmallservice.dto.UpdateLastLoginRequestDto;
 import com.nhnacademy.shoppingmallservice.service.AccountService;
 import com.nhnacademy.shoppingmallservice.service.CustomTokenService;
 import com.nhnacademy.shoppingmallservice.service.MemberAuthService;
+import com.nhnacademy.shoppingmallservice.webClient.MemberClient;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
@@ -20,6 +26,7 @@ public class AuthController {
     private final CustomTokenService tokenService;
     private final MemberAuthService memberAuthService;
     private final AccountService accountService;
+    private final MemberClient memberClient;
 
     @GetMapping("/")
     public String hi() {
@@ -37,9 +44,22 @@ public class AuthController {
         }
 
         String accessToken = tokenService.issueAccessAndRefreshToken(memberDto);
+
+        try {
+            UpdateLastLoginRequestDto updateLastLoginRequestDto = new UpdateLastLoginRequestDto(
+                    memberDto.email(),
+                    LocalDateTime.now()
+            );
+            memberClient.updateLastLogin(updateLastLoginRequestDto);
+        } catch (Exception e) {
+            log.error("마지막 로그인 업데이트 실패: {}", e.getMessage());
+        }
+
         LoginResponseDto successResponse = new LoginResponseDto(accessToken, memberDto.memberStateName(), null, memberDto.role());
         return ResponseEntity.status(HttpStatus.OK).body(successResponse);
+
     }
+
 
     @GetMapping("/api/auth/role")
     public ResponseEntity<String> getRoleFromToken(@RequestHeader("Authorization") String authorizationHeader){
